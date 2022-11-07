3cc3be4539df5d16c197b18e7582febfd932a222
Hassan <hassan@techbridge.com.pk>
2022-11-07 16:22:50 +0500

    category module part1

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,14 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::prefix('admin')->middleware('auth', 'isAdmin')->group(function(){
 
     Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
+
+    //Admin category routes
+    Route::controller(App\Http\Controllers\Admin\CategoryController::class)->group(function () {
+        Route::get('/category', 'index');
+        Route::get('/category/create', 'create');
+        Route::post('/category', 'store');
+        Route::get('/category/{category}/edit', 'edit');
+        Route::put('/category/{category}', 'update');
+    });
     
 });
